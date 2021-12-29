9d8a6746d2861f04efa49abf0fa59eb0d80a3144
liuyuan <373005226@qq.com>
2021-12-29 17:31:56 +0800

    init

---
+++ src/components/Test/index.tsx
@@ -0,0 +1,21 @@
+/**
+ * @author yizhi
+ * @date 2021-12-29 16:54
+ * @since 0.0.0
+ */
+
+import React from 'react'
+//import classnames from 'classnames'
+//import style from './style.module.scss'
+
+interface TestProps {}
+
+const Test: React.FC<TestProps> = props => {
+
+    return (
+        <div>
+        </div>
+    )
+}
+
+export default React.memo(Test)
